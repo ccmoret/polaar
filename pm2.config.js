c6cbbf15e96da6b2805f8bfac3717a7d79322814
cemoret <cemoret@gmail.com>
2019-04-03 17:47:22 -0300

    test

--- pm2.config.js
+++ pm2.config.js
@@ -4,7 +4,7 @@ module.exports = {
       instances: 1,
       max_restarts: 10,
       merge_logs: false,
-      script: './index.js',
+      script: 'start',
       max_memory_restart: '200M'
     }
   ]
