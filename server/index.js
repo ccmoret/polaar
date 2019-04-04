165fada22676549e87d0f4eff3bb22f648d511b1
cemoret <cemoret@gmail.com>
2019-04-04 20:13:50 -0300

    compression express server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
+const compression = require('compression');
 const express = require('express');
 const next = require('next');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
+
 const app = next({ dir: '.', dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  server.use(compression());
   server.get('*', (req, res) => {
     return handle(req, res);
   });
